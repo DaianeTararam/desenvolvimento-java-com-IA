939b69069847990a439d93d6f8e4885e9ac9f2dd
Daiane Tararam <daianetararam@example.com>
2024-06-12 17:50:43 -0300

    Terceiro commit

---
+++ condicionalComposta/src/ResultadoEscolar.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JOptionPane;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota;
+        nota = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor da nota:"));
+        if(nota >= 6){
+            JOptionPane.showMessageDialog(null,"Aluno APROVADO!");
+        }else{
+            JOptionPane.showMessageDialog(null,"Aluno REPROVADO!");
+        }
+    }
+}
---
+++ condicionalEncadeada/src/ResultadoEscolar.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota;
+        nota = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor da nota:"));
+        if(nota < 3){
+            JOptionPane.showMessageDialog(null,"Aluno REPROVADO!");
+        }else if(nota >= 3 && nota < 6 ){
+            JOptionPane.showMessageDialog(null,"Aluno em RECUPERAÇÃO!");
+        }else {
+            JOptionPane.showMessageDialog(null,"Aluno APROVADO!");
+        }
+    }
+}
+
